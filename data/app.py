3c62e2a573e4c3d91898b0e5e81e733d04605d98
kavin033 <rk.kavin2003@gmail.com>
2022-08-04 23:58:27 +0530

    deploy

--- data/app.py
+++ data/app.py
@@ -1,20 +1,21 @@
+from flask import Flask, render_template
 import os
-from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods = ['POST', 'GET'])
+
+@app.route('/')
 def home():
-    return render_template('./index.html')
+    return render_template('index.html')
 
 @app.route('/success', methods = ['POST', 'GET'])
 def search():
-    return render_template('./success.html')
+    return render_template('success.html')
 
 @app.route('/unsuccess', methods = ['POST', 'GET'])
 def callback():
-    return render_template('./unsuccess.html')
-          
-if __name__ == '__main__':
-   port = int(os.environ.get("PORT", 5000))
-   app.run(host="0.0.0.0", port=port)
+    return render_template('unsuccess.html')
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
