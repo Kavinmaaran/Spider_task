3313071ce9ef6e45ccd750e4e3bfde094839651a
kavin033 <rk.kavin2003@gmail.com>
2022-08-05 13:53:37 +0530

    deploy

--- data/app.py
+++ data/app.py
@@ -1,12 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask,render_template, request
 import os
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'db'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'example'
+app.config['MYSQL_DB'] = 'form'
+ 
+mysql = MySQL(app)
 
 
-@app.route('/')
+@app.route('/', methods = ['POST', 'GET'])
 def home():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    if request.method == 'POST':
+        try:
+            name = request.form['name']
+            dept = request.form['dept']
+            email = request.form['email']
+            rollno = request.form['rollno']
+            cursor = mysql.connection.cursor()
+            cursor.execute('''INSERT INTO `form_details` (`name`, `dept`, `email`, `rollno`)
+                            VALUES (%s, %s, %s, %s);''',(name,dept,email,rollno))
+            mysql.connection.commit()
+            cursor.close()
+            return render_template('success.html')
+        except:
+            cursor.close()
+            return render_template('unsuccess.html')
 
 @app.route('/success', methods = ['POST', 'GET'])
 def search():
@@ -19,3 +42,5 @@ def callback():
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
+ 
+ 
